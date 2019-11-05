5e2d0fe9bddd50d7690c84a526326997eee19f24
David Schoenholtz <dmschoenholtz@gmail.com>
2019-11-05 12:27:33 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("#id-checkbox").addEventListener("submit", function(event) {
+         document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
+         event.preventDefault();
+}, false);
 });
