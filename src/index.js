a33601f5f13b09f1cf841ca1e6661871a42e8929
David Schoenholtz <dmschoenholtz@gmail.com>
2019-11-05 12:27:56 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#id-checkbox").addEventListener("submit", function(event) {
-         document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
          event.preventDefault();
-}, false);
+}
 });
